9218a9c326c29781285a79512d4660e157beb739
racagogi <racagogi@gmail.com>
2025-02-12 10:34:39 +0900

    remove unsued path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,33 +7,6 @@ if status is-interactive
 	fish_add_path $HOME/.ghcup/bin/
 	fish_add_path $HOME/.cabal/bin/
 	fish_add_path $HOME/.local/bin/
-	fish_add_path $HOME/.local/fish/bin/
-	fish_add_path $HOME/.local/chez/bin/
-	fish_add_path $HOME/.local/chicken/bin/
-	fish_add_path $HOME/.local/clojure/bin/
-	fish_add_path $HOME/.local/cmake/bin/
-	fish_add_path $HOME/.local/dotnet/bin/
-	fish_add_path $HOME/.local/erlang/bin/
-	fish_add_path $HOME/.local/elixir/bin/
-	fish_add_path $HOME/.local/go/bin/
-	fish_add_path $HOME/.local/idiris2/bin/
-	fish_add_path $HOME/.local/jdk/bin/
-	fish_add_path $HOME/.local/julia/bin/
-	fish_add_path $HOME/.local/maven/bin/
-	fish_add_path $HOME/.local/lua-language-server/bin/
-	fish_add_path $HOME/.local/opam/bin/
-	fish_add_path $HOME/.local/python/bin/
-	fish_add_path $HOME/.local/ruby/bin/
-	fish_add_path $HOME/.local/racket/bin/
-	fish_add_path $HOME/.local/share/coursier/bin/
-	fish_add_path $HOME/.local/node/bin/
-	fish_add_path $HOME/.local/wezterm/target/release
-	fish_add_path $HOME/.local/eww/target/release
-	fish_add_path $HOME/.local/rio/target/release
-	fish_add_path $HOME/.local/swww/target/release
-	fish_add_path $HOME/.local/niri/target/release
-	set -x GOPATH $HOME/.local/go/go
-	set -x DOTNET_ROOT $HOME/.local/share/dotnet
 	set -x MANPAGER "nvim +Man!"
 	set -x EDITOR "nvim"
 	set -x PAGER "nvim -R -c 'nnoremap q :lclose<CR>:q<CR>'"
