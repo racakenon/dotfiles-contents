d9da14d6f694117c4a88c6875e669d2afdd4da6e
racagogi <racagogi@gmail.com>
2025-02-24 16:04:01 +0900

    nvim use snip to autopair

--- .config/nvim/lua/plugins/snip/luasnip.lua
+++ .config/nvim/lua/plugins/snip/luasnip.lua
@@ -4,5 +4,6 @@ return {
 	dependencies = { "rafamadriz/friendly-snippets" },
 	config = function()
 		require("luasnip.loaders.from_vscode").lazy_load()
+		require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/snippets/" })
 	end
 }
