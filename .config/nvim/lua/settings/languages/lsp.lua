d3945a807e9fbb3603f62f0d93d596a19e575e5f
racagogi <racagogi@gmail.com>
2025-03-29 22:47:00 +0900

    update neovim 0.11

--- .config/nvim/lua/settings/languages/lsp.lua
+++ .config/nvim/lua/settings/languages/lsp.lua
@@ -1,21 +1,28 @@
 vim.api.nvim_create_autocmd('LspAttach', {
 	callback = function(ev)
-		local opts = { win = ev.win, buf = ev.buf }
+		local opts = { buf = ev.buf }
+		vim.keymap.del('n', 'K', { buffer = ev.buf })
+		vim.keymap.del('n', 'grn')
+		vim.keymap.del('n', 'gra')
+		vim.keymap.del('n', 'grr')
+		vim.keymap.del('n', 'gri')
+		vim.keymap.del('n', 'gO')
+		vim.keymap.del('i', '<C-S>')
 		vim.api.nvim_set_option_value("formatexpr", "v:lua.vim.lsp.formatexpr()", opts)
 		vim.api.nvim_set_option_value("omnifunc", "v:lua.vim.lsp.omnifunc", opts)
 		vim.api.nvim_set_option_value("tagfunc", "v:lua.vim.lsp.tagfunc", opts)
 		opts = { buffer = ev.buf }
 		vim.keymap.set('n', '<space>d', vim.lsp.buf.definition, opts)
 		vim.keymap.set('n', '<space>n', vim.lsp.buf.rename, opts)
-		vim.keymap.set('n', '<space>h', function ()
-			vim.lsp.buf.hover()
+		vim.keymap.set('n', '<space>h', function()
+			vim.lsp.buf.hover({ border = "rounded", focus = true, max_width = 80 })
 		end, opts)
 		vim.keymap.set('n', '<space>r', vim.lsp.buf.references, opts)
 		vim.keymap.set('n', '<space>i', vim.lsp.buf.implementation, opts)
 		vim.keymap.set('n', '<space>D', vim.lsp.buf.declaration, opts)
 		vim.keymap.set({ 'n', 'v' }, '<space>a', vim.lsp.buf.code_action, opts)
 		vim.keymap.set('n', '<space>r', vim.lsp.buf.references, opts)
-		vim.keymap.set('n', '<space>s', vim.lsp.buf.signature_help, opts)
+		vim.keymap.set({ 'n', 'i' }, '<space>s', vim.lsp.buf.signature_help, opts)
 		vim.keymap.set('n', '<space>w', vim.lsp.buf.add_workspace_folder, opts)
 		vim.keymap.set('n', '<space>f', function()
 			vim.lsp.buf.format { async = true }
@@ -27,11 +34,11 @@ vim.keymap.del('n', ']d')
 vim.keymap.del('n', '[d')
 
 vim.keymap.set('n', ']d', function()
-	vim.diagnostic.goto_next({ float = true })
+	vim.diagnostic.jump({ count = 1, float = true })
 end, { desc = 'Jump to the next diagnostic' })
 
 vim.keymap.set('n', '[d', function()
-	vim.diagnostic.goto_prev({ float = true })
+	vim.diagnostic.jump({ count = -1, float = true })
 end, { desc = 'Jump to the previous diagnostic' })
 
 vim.api.nvim_create_user_command('LspLog',
@@ -44,6 +51,7 @@ vim.api.nvim_create_user_command('LspLog',
 )
 
 vim.diagnostic.config({
+	virtual_text = true,
 	float = {
 		border = "rounded"
 	},
@@ -63,14 +71,6 @@ function M.setup(filetype, name, root_files, cmd, settings)
 					root_dir = root_dir,
 					capabilities = vim.tbl_deep_extend('keep', vim.lsp.protocol.make_client_capabilities(),
 						require('cmp_nvim_lsp').default_capabilities()),
-					handlers = {
-						['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
-							style = "minmal",
-							max_width = 80,
-							border = "rounded",
-							focuse = true,
-						})
-					}
 				}, settings))
 			end
 		end
