92a49253309aff4332c432184e36805b8debe420
racakenon <racakenon@gmail.com>
2025-09-10 14:59:07 +0900

    fix(help): help now overide h command

--- .config/nvim/lua/settings/utils/floating-help.lua
+++ .config/nvim/lua/settings/utils/floating-help.lua
@@ -1,33 +1,25 @@
-vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
-	callback = function(args)
-		local buf = args.buf
-		if buf ~= nil and vim.bo[buf].filetype == 'help' then
-			local current_help_win = vim.api.nvim_get_current_win()
-			local opts = {
-				width = 80,
-				height = vim.opt.lines:get() - 6,
-				window_opts = {
-					border = 'rounded',
-					style = 'minimal',
-					anchor = 'NW',
-					relative = 'editor',
-					offset_x = (vim.opt.columns:get() - 78) / 2,
-					offset_y = 0,
-					zindex = 49,
-					external = false,
-					focusable = true,
-					noautocmd = true,
-				}
-			}
-			local float_opts = vim.lsp.util.make_floating_popup_options(opts.width, opts.height, opts.window_opts)
+vim.api.nvim_create_user_command('Help', function(opts)
+	vim.cmd('help ' .. opts.args)
+	local buf = vim.api.nvim_get_current_buf()
+	vim.cmd.close() -- Immediately close the split window
 
-			local help_win = vim.api.nvim_open_win(buf, true, float_opts)
+	local width = math.min(80, vim.o.columns - 4)
+	local height = math.min(80, vim.o.lines - 6)
 
-			vim.api.nvim_win_set_height(help_win, opts.height)
-
-			vim.wo[help_win].scroll = vim.wo[current_help_win].scroll
-
-			vim.api.nvim_win_close(current_help_win, false)
-		end
-	end
+	vim.api.nvim_open_win(buf, true, {
+		relative = 'editor',
+		width = width,
+		height = height,
+		col = (vim.o.columns - width) / 2,
+		row = (vim.o.lines - height) / 2,
+		border = 'rounded',
+		style = 'minimal'
+	})
+end, {
+	nargs = 1,
+	complete = 'help',
+	desc = 'Open help in floating window'
 })
+
+vim.cmd('cnoreabbrev h Help')
+vim.cmd('cnoreabbrev help Help')
