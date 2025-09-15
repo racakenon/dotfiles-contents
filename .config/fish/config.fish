00bfc13fe97d6bd132b33539e212aed4ff8242c8
racakenon <racakenon@gmail.com>
2025-09-15 13:48:39 +0900

    feat(opam): add opam env

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -104,3 +104,12 @@ if status is-login
 	end
 end
 
+
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/home/raca/.opam/opam-init/init.fish' && source '/home/raca/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
